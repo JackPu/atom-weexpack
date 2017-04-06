ac826462081f2bfbf2fb58c853b5259f893b9912
JackPu <kakashjack@gmail.com>
2017-04-06 14:14:39 +0800

    add files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# atom-weexpack
+# atom-weexpack package
+
 an atom plugin for weexpack
+
+you can create and build a weex porject by this plugin
---
+++ spec/atom-weexpack-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomWeexpackView from '../lib/atom-weexpack-view';
+
+describe('AtomWeexpackView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
